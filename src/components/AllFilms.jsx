36207498c6bcc2f706fbfdbb0cf278727ac96d06
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-09-17 12:44:50 -0400

    AllFilms import

---
+++ src/components/AllFilms.jsx
@@ -0,0 +1,6 @@
+import React, { useEffect, useState } from 'react'
+import Group from './Group'
+import "./global.css"
+import { Route, Routes } from 'react-router-dom'
+import Film from './Film'
+import Home from './Home'
