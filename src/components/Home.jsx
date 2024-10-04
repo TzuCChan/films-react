42b0c3ce412b585eb17561ce88c82375f9acce03
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-04 15:32:12 -0400

    home.jsx import

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+      <p>This is 7k Movie your sight to look up your favorite
+        movie
+      </p>
+    </div>
+  )
+}
