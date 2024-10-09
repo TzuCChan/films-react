9b9d7647491d77a00869f36f3b19d93cbb7a26cb
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-09 14:27:05 -0400

    export default function AllFilms

--- src/components/AllFilms.jsx
+++ src/components/AllFilms.jsx
@@ -4,3 +4,9 @@ import "./global.css"
 import { Route, Routes } from 'react-router-dom'
 import Film from './Film'
 import Home from './Home'
+
+export default function AllFilms({ data }) {
+  const [organizedFilmss, setOrganizedFilmss] = useState([])
+  const [showList, setShowList] = useState(false)
+  const [films, setFilms] = useState([])
+}
