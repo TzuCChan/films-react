16c065dd8f084e14e54aad8ea448744d47dde786
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-18 14:30:53 -0400

    AllFilms.jsx useEffect

--- src/components/AllFilms.jsx
+++ src/components/AllFilms.jsx
@@ -6,10 +6,29 @@ import Film from './Film'
 import Home from './Home'
 
 export default function AllFilms({ data }) {
-  const [organizedFilmss, setOrganizedFilmss] = useState([])
+  const [organizedFilms, setOrganizedFilms] = useState([])
   const [showList, setShowList] = useState(false)
   const [films, setFilms] = useState([])
 
   const propComparator = (propName) =>
     (a, b) => a[propName].toLowerCase() === b[propName].toLowerCase() ? 0 : a[propName].toLowerCase() < b[propName].toLowerCase() ? -1 : 1
+
+  
+  useEffect(() => {
+  
+    data.sort(propComparator(`title`))
+
+    let grouped = data.reduce((r, e) => {
+      let group = e.title[0];
+      if(!r[group]) r[group] = {group, children: [e]}
+      else r[group].children.push(e);
+      return r;
+    }, {})
+
+    let result = Object.values(grouped)
+    setOrganizedFilms(result)
+
+
+  }, [data])
+
 }
