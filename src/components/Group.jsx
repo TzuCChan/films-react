7bc6d41e41e9d1e3edab4358c1b61f75d108d210
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-23 14:45:52 -0400

    function Group

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -2,3 +2,10 @@ import { Link } from "react-router-dom"
 import { useState } from "react";
 import "./global.css"
 import Film from "./Film";
+
+export default function Group({ showList, data }) {
+  const [modal, setModal] = useState(false)
+  const [Film, setFilm] = useState("")
+  
+  const Toggle = () => setModal(!modal);
+}
