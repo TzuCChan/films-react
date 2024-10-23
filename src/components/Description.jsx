1864a1776ede6b5c7ae5dca88f875c667f991f70
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-23 14:30:43 -0400

    function Description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,1 +1,10 @@
 import React from 'react'
+
+export default function Description(showModal, setShowModal, overview) {
+  console.log(overview)
+  return (
+  <div>
+    {showModal ? <div>description: {overview}</div> : null}
+  </div>
+  )
+}
