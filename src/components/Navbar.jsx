338152bd419f35b1586cd05358ac8290d68f18ad
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-28 11:25:45 -0400

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,2 +1,15 @@
 import { Link } from 'react-router-dom'
 import "./global.css"
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <h1 className='title'>7k Films</h1>
+        <nav className='nav-container'>
+          <Link to="/all-films"><button>All Films</button></Link>
+        </nav>
+      </header>
+    </div>
+  )
+}
