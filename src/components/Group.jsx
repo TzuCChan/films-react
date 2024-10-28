314096150cbcc2e98acd30ad7d5036561512b926
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-28 11:18:50 -0400

    return

--- src/components/Group.jsx
+++ src/components/Group.jsx
@@ -12,8 +12,33 @@ export default function Group({ showList, data }) {
   const handleChange = (event) => {
     data.map((film) => {
       if (film.title.toUpperCase() === event.target.value.toUpperCase()) { 
-        setMovie(film)
+        setFilm(film)
       }
     })
   }
+
+  return <div>{showList ? film ?
+    <div className="list-container">
+      <Link to={`/all-films/${film.id}`}>
+        <button className="one-film" onClick={() => Toggle()}>{film.title}</button>
+      </Link>
+    </div>
+    :
+    <div> 
+      <ul>
+        <input type='text' placeholder='Search for Film' onChange={handleChange}>
+        
+        </input>
+        {data.map((film) => {
+          return (
+            
+            <li>
+            <Link to={`/all-films/${film.id}`}>
+              <button onClick={() => Toggle()}>{film.title}</button>
+            </Link> 
+            </li>
+          )  
+        })}
+      </ul>
+    </div> : null}</div>
 }
