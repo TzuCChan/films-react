a02aad24308a4f8cd44711169aebef5e3b4fb31d
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-28 11:38:31 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import AllFilms from "./components/AllFilms";
+import Film from "./components/Film";
