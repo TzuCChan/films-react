363a6dcf766b4327de9ce26be26d8487a61fc13f
Leon Tzu Chiang Chan <tzuchiangchan@gmail.com>
2024-10-30 11:02:31 -0400

    app.js function app

--- src/App.js
+++ src/App.js
@@ -6,3 +6,17 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import AllFilms from "./components/AllFilms";
 import Film from "./components/Film";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  const getData = async () => {
+    const response = await axios.get(
+      "https://ga-movies-api.herokuapp.com/ids",
+      {
+        mode: "no-cors",
+      }
+    );
+    setData(response.data);
+  }
+}
